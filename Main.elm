ca34c51aa02a1616ff2d124fe75ffc3c521cf189
vikas agartha <vikasagartha@gmail.com>
2016-10-10 14:55:38 -0700

    restart

--- Main.elm
+++ Main.elm
@@ -1,114 +1,62 @@
 module Main exposing (..)
 
-import Html.App
 import Html
+import Html.App
 import Html.Attributes
 
-borderColor : String
-borderColor =
-    "tan"
 
-type alias Model =
-    { canvas : Size
-    , borderSize : Int
-    , frame : Frame
-    }
-
-type alias Size =
-    { width : Int, height : Int }
-
-type Frame
-    = SingleImage { url : String }
-    | HorizontalSplit 
-        { top : Frame 
-        , topHeight : Int
-        , bottom : Frame 
-        }
+--Init 
+type alias Model = ()
 
 initialModel : Model
-initialModel = 
-    { canvas = { width = 250, height =  250}
-    , borderSize= 5 
-    , frame = 
-        HorizontalSplit 
-        { top = SingleImage { url = "http://i.imgur.com/bjjypBA.jpg" }
-        , topHeight = 80 
-        , bottom = SingleImage { url = "http://imgur.com/K02jg2O.jpg" }
-        }
-        
-    }
+initialModel =
+    ()
 
+--Update
 type Msg = NothingYet
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     (model, Cmd.none)
 
-viewCanvas : Int -> Size -> Frame -> Html.Html Msg
-viewCanvas borderSize size rootFrame = 
+--View
+viewCanvas : Html.Html Msg
+viewCanvas =
     Html.div 
-    [ Html.Attributes.style
-        [ ("width", toString size.width ++ "px")
-        , ("height", toString size.height ++ "px")
-        , ("border", "2px solid black")
-        , ("border", "2px solid black")
-        ]
-    ]
-    [ Html.div 
         [ Html.Attributes.style 
-            [("border", toString borderSize ++ "px solid tan")]
+            [ ("width", "250px") 
+            , ("height", "250px") 
+            , ("border", "2px solid black") 
             ]
-      [viewFrame 
-        borderSize
-        { width = size.width - 2*borderSize
-        , height = size.height - 2*borderSize 
-        }
-        rootFrame
-      ]
-    ]
-
-viewFrame : Int -> Size -> Frame -> Html.Html Msg
-viewFrame borderSize size frame = 
-    case frame of
-        SingleImage { url } ->
-            Html.div
-                [ Html.Attributes.style 
-                    [ ("width", toString size.width ++ "px")
-                    , ("height", toString size.height ++ "px")
-                    , ("background-image", "url("++url++ ")")
-                    , ("background-size", "auto"++ toString size.height ++"px")
-                    ]
+        ]
+        [ Html.div
+            [ Html.Attributes.style 
+                [ ("height", "250px") 
+                , ("background-image", "url(http://i.imgur.com/bjjypBA.jpg)") 
+                , ("background-size", "auto 250px") 
                 ]
-                []
-        HorizontalSplit {top, topHeight, bottom} ->
-            Html.div []
-            [ viewFrame borderSize { width = size.width, height = topHeight } top 
-            , Html.div 
-                [ Html.Attributes.style 
-                    [ ("width", toString size.width ++ "px") 
-                    , ("height", toString borderSize ++ "px") 
-                    , ("background-color", borderColor) 
-                    ]
-                ]
-                []
-            , viewFrame borderSize { width = size.width, height = (size.height - topHeight - borderSize ) } bottom
             ]
+            []
+        ]
 
 view : Model -> Html.Html Msg
 view model =
     Html.div 
-    [ Html.Attributes.style [("padding", "8px")]
-    ]
-    [ viewCanvas model.borderSize model.canvas model.frame
-    , Html.hr [] []
-    , Html.text <| toString model
-    ]
+        [ Html.Attributes.style 
+            [ ("padding", "8px") 
+            ]
+        ]
+        [ viewCanvas
+        , Html.hr [] []
+        , Html.text <| toString model 
+        ]
 
-main =
-    Html.App.program
-        {
-            init = (initialModel, Cmd.none),
-            subscriptions = \_ -> Sub.none,
-            update = update,
-            view = view
-        }
+subscriptions =
+    Sub.none
+
+main = Html.App.program
+    { init = (initialModel, Cmd.none)
+    , subscriptions = \_ -> Sub.none
+    , update =  update 
+    , view =  view 
+    }
